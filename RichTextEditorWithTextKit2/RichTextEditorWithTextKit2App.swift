6a777d4effd396f47580c94745bfa3fb6603bef0
Koo Bonwook <bnoo1333@gmail.com>
2025-01-28 18:12:05 +0900

    add Project

---
+++ RichTextEditorWithTextKit2/RichTextEditorWithTextKit2App.swift
@@ -0,0 +1,17 @@
+//
+//  RichTextEditorWithTextKit2App.swift
+//  RichTextEditorWithTextKit2
+//
+//  Created by 구본욱 on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct RichTextEditorWithTextKit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
